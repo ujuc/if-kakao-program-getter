c52a0e7abccfacf9b56b5cb3e001dcc2975f0da7
Thomas SJ Kang <ujuc@ujuc.kr>
2019-09-22 15:44:02 +0900

    Change Json package
    이터레이터가 안되서 바꿔봄

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use std::io;
 use std::fs;
-use serde_json::Value;
 
 /// 파일에서 정보를 가져온다.
-fn get_json_content_on_file() -> Result<Value, io::Error> {
-    let file_contents = fs::read_to_string("data/program.json")?;
-    let contents: Value = serde_json::from_str(&file_contents).unwrap();
+fn get_json_content_on_file() -> json::JsonResult<json::JsonValue> {
+    let file_contents = fs::read_to_string("data/program.json").unwrap();
 
-    Ok(contents)
+    json::parse(&file_contents)
 }
 
 fn main() -> Result<(), io::Error> {
-    let contents = get_json_content_on_file()?;
+    let contents = get_json_content_on_file().unwrap();
 
     println!("{:?}", contents);
     Ok(())
