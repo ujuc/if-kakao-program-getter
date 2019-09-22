d3f4e3f3908cea50410ef3627f2323abeeb4a329
Thomas SJ Kang <ujuc@ujuc.kr>
2019-09-22 14:25:07 +0900

    Get program data on json file (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::io;
+use std::fs;
+use serde_json::Value;
+
+/// 파일에서 정보를 가져온다.
+fn get_json_content_on_file() -> Result<Value, io::Error> {
+    let file_contents = fs::read_to_string("data/program.json")?;
+    let contents: Value = serde_json::from_str(&file_contents).unwrap();
+
+    Ok(contents)
+}
+
+fn main() -> Result<(), io::Error> {
+    let contents = get_json_content_on_file()?;
+
+    println!("{:?}", contents);
     Ok(())
 }
